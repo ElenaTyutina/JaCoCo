ebc765cc7aa7ceacc535d4b6cb660ad43dbf87d0
Elena Tuytina <yptyutina@mail.ru>
2021-09-21 22:48:52 +0300

    Fix ci2

--- Statistics/src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ Statistics/src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -11,9 +11,9 @@ class StatisticsServiceTest {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 12;
+        long expected = 3;
 
-        long actual = service.findMax(incomesInBillions);
+        long actual = service.findMin(incomesInBillions);
 
         assertEquals(expected, actual);
     }
